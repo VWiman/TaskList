20e87da82b8294ca413236ac5c9c8fa1fa4bac4b
why_code <119089018+VWiman@users.noreply.github.com>
2024-05-28 10:11:15 +0200

    Added sections

--- app/(app)/index.jsx
+++ app/(app)/index.jsx
@@ -1,12 +1,23 @@
 import { ItemsContext } from "@/context/ItemsContext";
 import { useRouter } from "expo-router";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Text, View, FlatList, Pressable, StyleSheet } from "react-native";
 import { FontAwesome6 } from "@expo/vector-icons";
 
 export default function Index() {
 	const { items } = useContext(ItemsContext);
 	const router = useRouter();
+
+	const [undoneItems, setUndoneItems] = useState([]);
+	const [doneItems, setDoneItems] = useState([]);
+
+	useEffect(() => {
+		const newUndone = items.filter((item) => item.isDone === false);
+		setUndoneItems(newUndone);
+		const newdone = items.filter((item) => item.isDone === true);
+		setDoneItems(newdone);
+	}, [items]);
+
 	return (
 		<View
 			style={{
@@ -16,14 +27,52 @@ export default function Index() {
 				backgroundColor: "#cddbea",
 			}}>
 			<FlatList
-				style={{ paddingVertical: 24, paddingHorizontal: 24 }}
-				data={items}
+				ListHeaderComponent={() => <Text style={styles.header}>TO DO</Text>}
+				style={{
+					paddingVertical: 12.5,
+					paddingHorizontal: 12.5,
+					height: "50%",
+					width: "100%",
+				}}
+				data={undoneItems}
 				keyExtractor={(item) => item.id.toString()}
 				renderItem={({ item }) => (
 					<Pressable
-						onPress={() => router.push(`/${item.id}`)}
+						onPress={() => {
+							router.push(`/${item.id}`);
+						}}
 						style={({ pressed }) => [pressed ? styles.buttonPressed : styles.button]}>
-						{item.isDone ? <Text style={styles.textDone}> {item.title}</Text>  : <Text style={styles.text}> {item.title}</Text>}
+						{item.isDone ? (
+							<Text style={styles.textDone}> {item.title}</Text>
+						) : (
+							<Text style={styles.text}> {item.title}</Text>
+						)}
+						<FontAwesome6 name="chevron-right" size={16} color="white" />
+					</Pressable>
+				)}
+			/>
+			<View style={{ borderColor: "#6d9ac3", borderWidth: 4, width: "100%", zIndex: 100 }}></View>
+			<FlatList
+				ListHeaderComponent={() => <Text style={styles.header}>DONE</Text>}
+				style={{
+					paddingVertical: 12.5,
+					paddingHorizontal: 12.5,
+					height: "50%",
+					width: "100%",
+				}}
+				data={doneItems}
+				keyExtractor={(item) => item.id.toString()}
+				renderItem={({ item }) => (
+					<Pressable
+						onPress={() => {
+							router.push(`/${item.id}`);
+						}}
+						style={({ pressed }) => [pressed ? styles.buttonPressed : styles.button]}>
+						{item.isDone ? (
+							<Text style={styles.textDone}> {item.title}</Text>
+						) : (
+							<Text style={styles.text}> {item.title}</Text>
+						)}
 						<FontAwesome6 name="chevron-right" size={16} color="white" />
 					</Pressable>
 				)}
@@ -69,6 +118,17 @@ const styles = StyleSheet.create({
 		elevation: 0,
 		shadowRadius: 8,
 		shadowOffset: { width: 1, height: 13 },
+	},
+	header: {
+		fontSize: 19,
+		fontWeight: "bold",
+		color: "#396590",
+		marginBottom: 12.5,
+		letterSpacing: 0.15,
+		margin: 24,
+		textAlign: "left",
+		justifyContent: "flex-start",
+		width: "80%",
 	},
 	text: {
 		fontSize: 16,
